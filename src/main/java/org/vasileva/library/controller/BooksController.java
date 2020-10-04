5dc5537423cc92caaa6c16976056b46e2cd49a16
Elena Vasileva <elenavw@gmail.com>
2020-10-04 21:51:32 +0400

    commit 4

--- src/main/java/org/vasileva/library/controller/BooksController.java
+++ src/main/java/org/vasileva/library/controller/BooksController.java
@@ -2,37 +2,58 @@ package org.vasileva.library.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.vasileva.library.model.Books;
 import org.vasileva.library.service.BooksService;
 
+
 @Controller
+@RequestMapping(value = "/books/")
 public class BooksController {
 
-    @Autowired
-    BooksService booksService;
+    private BooksService booksService;
 
-    @GetMapping (value = "/{id}")
-    public String getBook (@PathVariable (value = "id") Long id) {
-        return "get_book";
+    @Autowired
+    public BooksController(BooksService booksService) {
+        this.booksService = booksService;
     }
 
-    @PostMapping (value = "")
+    @GetMapping
+    public String showAllBooks (Model model) {
+        model.addAttribute("books", booksService.getAll());
+        return "index";
+    }
+
+    @GetMapping (value = "{id}")
+    public String showBook (@PathVariable (value = "id") Long id, Model model) {
+        model.addAttribute("book", booksService.getById(id));
+        return "show";
+    }
+
+/*    @PostMapping (value = "")
     public String saveBook (@RequestBody Books book) {
         return "save_book";
     }
 
-    @PutMapping (value = "/{id}")
+    @PutMapping (value = ":id")
     public String updateBook (@RequestBody Books bookDetails, @PathVariable (value = "id") Long id) {
         return "put_book";
     }
 
-    @DeleteMapping (value = "/{id}")
+    @DeleteMapping (value = ":id")
     public String deleteBook (@PathVariable (value = "id") Long id) {
         return "delete_book";
     }
-    @GetMapping (value = "")
-    public String getAllBooks (@PathVariable (value = "id") Long id) {
-        return "get_all_books";
+
+
+    @GetMapping (value = "new")
+    public String newform () {
+        return "";
     }
+
+    @GetMapping (value = ":id/edit")
+    public String editform () {
+        return "";
+    }*/
+
 }
